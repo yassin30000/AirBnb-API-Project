c18bcf74a72b629873749dbfb890a50e1302ee88
Yassin Tantawy <yassintantawy3@gmail.com>
2023-06-10 13:45:05 -0700

    commented out options object

--- backend/db/seeders/20230609151551-demo-spot.js
+++ backend/db/seeders/20230609151551-demo-spot.js
@@ -1,9 +1,9 @@
 'use strict';
 
-let options = {};
-if (process.env.NODE_ENV === 'production') {
-    options.schema = process.env.SCHEMA;  // define your schema in options object
-}
+// let options = {};
+// if (process.env.NODE_ENV === 'production') {
+//     options.schema = process.env.SCHEMA;  // define your schema in options object
+// }
 
 /** @type {import('sequelize-cli').Migration} */
 
