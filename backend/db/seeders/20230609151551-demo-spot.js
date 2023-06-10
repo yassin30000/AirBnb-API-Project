4f15f6d989c583c63d7e0480bdb9ebc9b9b68673
Yassin Tantawy <yassintantawy3@gmail.com>
2023-06-10 12:50:45 -0700

    add create-spot feature

---
+++ backend/db/seeders/20230609151551-demo-spot.js
@@ -0,0 +1,37 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+    options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        options.tableName = 'Spots';
+        return queryInterface.bulkInsert(options, [
+            {
+                address: "1414 Hello Ln",
+                ownerId: 1,
+                city: "Walnut Creek",
+                state: "CA",
+                country: "United States",
+                lat: 37.7645358,
+                lng: -122.4730327,
+                name: "demoSpot1",
+                description: "paldsas",
+                price: 1344
+            }
+        ], {});
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        options.tableName = 'Spots';
+        const Op = Sequelize.Op;
+        return queryInterface.bulkDelete(options, {
+            name: { [Op.in]: ['demoSpot1'] }
+        }, {});
+    }
+};
