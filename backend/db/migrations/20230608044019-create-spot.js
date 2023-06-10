cea370b6c9d468a8ae40da9b38f502e6d3ab867a
Yassin Tantawy <yassintantawy3@gmail.com>
2023-06-10 13:57:31 -0700

    changed every options to SPOTS

--- backend/db/migrations/20230608044019-create-spot.js
+++ backend/db/migrations/20230608044019-create-spot.js
@@ -2,10 +2,10 @@
 
 /** @type {import('sequelize-cli').Migration} */
 
-let options = {};
-if (process.env.NODE_ENV === 'production') {
-    options.schema = process.env.SCHEMA;  // define your schema in options object
-}
+// let options = {};
+// if (process.env.NODE_ENV === 'production') {
+//     options.schema = process.env.SCHEMA;  // define your schema in options object
+// }
 
 module.exports = {
     async up(queryInterface, Sequelize) {
@@ -76,10 +76,10 @@ module.exports = {
                 type: Sequelize.DATE,
                 defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
             }
-        }, options);
+        } /*options*/);
     },
     async down(queryInterface, Sequelize) {
-        options.tableName = "Spots";
-        return queryInterface.dropTable(options);
+        // options.tableName = "Spots";
+        return queryInterface.dropTable('Spots');
     }
 };
