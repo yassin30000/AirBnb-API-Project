cefcfd3da9a3da2d2f437a3ddaee84768b525410
Yassin Tantawy <yassintantawy@Yassins-MacBook-Pro.local>
2023-07-22 11:45:42 -0700

    nav

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,17 @@
+const Carrot = () => {
+    return (
+        <div style={{ color: "orange", fontSize: "100px" }}>
+            <i className="fas fa-carrot"></i>
+        </div>
+    );
+};
+
+function ProfileButton() {
+    return (
+        <div style={{ color: "black", fontSize: "50px" }}>
+            <i className="fas fa-user-circle" />
+        </div>
+    )
+}
+
+export default ProfileButton;
